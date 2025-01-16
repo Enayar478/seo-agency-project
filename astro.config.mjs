6c78e17e936e49b93c6735ac785e3d7acd19f923
RG <enayar.gh@protonmail.com>
2025-01-16 17:35:21 +0100

    Add Vercel adapter for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://votre-agence.com',
   integrations: [
     tailwind(),
   ],
+  output: 'server',
+  adapter: vercel(),
 }); 
