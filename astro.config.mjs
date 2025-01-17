ce13bd7caff62a4dbb749efb20dd3c8a8ed436dd
Enayar <rayane.g@proton.me>
2025-01-17 17:28:36 +0100

    correction erreur déploiement vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
+import speedInsights from '@vercel/speed-insights/astro';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'http://localhost:4321',
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    speedInsights()
+  ],
   output: 'static',
-  build: {
-    format: 'directory'
-  }
+  adapter: vercel(),
 }); 
