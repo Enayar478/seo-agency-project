aea8bee61baf9cc8f77b3010294aa3d5c2f08cf1
Enayar <rayane.g@proton.me>
2025-01-17 17:32:58 +0100

    errors correction

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/static';
-import speedInsights from '@vercel/speed-insights/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://localhost:4321',
-  integrations: [
-    tailwind(),
-    speedInsights()
-  ],
   output: 'static',
   adapter: vercel(),
+  integrations: [tailwind()]
 }); 
